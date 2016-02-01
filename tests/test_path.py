854675aa7f973eb3550cd437407a0592c5db2533
Basil Veerman <bveerman@uvic.ca>
2016-02-01 14:10:07 -0800

    Add test for model grouping

--- tests/test_path.py
+++ tests/test_path.py
@@ -10,3 +10,9 @@ from pyclimate import group_files_by_model_set, iter_matching_cmip5_file
 def test_file_iter(cmip5_file_list, _filter, expected):
     i = iter_matching_cmip5_file(cmip5_file_list, _filter)
     assert len(list(i)) == expected
+
+def test_model_grouping(cmip5_file_list):
+    groups = group_files_by_model_set(cmip5_file_list)
+    assert len(groups) == 6
+    assert 'CanESM2_rcp45_r1i1p1' in groups.keys()
+    assert len(groups['CanESM2_rcp45_r1i1p1']) == 2
