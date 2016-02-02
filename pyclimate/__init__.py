147007b05166aef21469052f7209b95ee3edb63c
Basil Veerman <bveerman@uvic.ca>
2016-02-01 20:56:51 -0800

    Add base Consumer class for multiprocessing

--- pyclimate/__init__.py
+++ pyclimate/__init__.py
@@ -1,5 +1,6 @@
 import os
 import fnmatch
+import multiprocessing
 
 from collections import defaultdict
 
@@ -162,6 +163,25 @@ class Cmip3File():
     def fullpath(self):
         return os.path.join(self.dirname, self.basename)
 
+class Consumer(multiprocessing.Process):
+
+    def __init__(self, task_queue, result_queue):
+        multiprocessing.Process.__init__(self)
+        self.task_queue = task_queue
+        self.result_queue = result_queue
+
+    def run(self):
+        proc_name = self.name
+        while True:
+            next_task = self.task_queue.get()
+            if next_task is None:
+                # Poison Pill says exit
+                print '%s: Exiting' % proc_name
+                break
+            print '{}: {}'.format(proc_name, next_task)
+            answer = next_task()
+            self.result_queue.put(answer)
+        return
 
 def iter_netcdf_files(base_dir, pattern="*.nc"):
     for root, dirnames, filenames in os.walk(base_dir):
