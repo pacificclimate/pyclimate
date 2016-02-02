ae84aa5280ad0c04a1dd3eeddfaeec0d72b1b463
Basil Veerman <bveerman@uvic.ca>
2016-02-02 15:30:42 -0800

    Fix path test imports

--- tests/test_path.py
+++ tests/test_path.py
@@ -1,11 +1,11 @@
 import pytest
 
-from pyclimate import group_files_by_model_set, iter_matching_cmip5_file
+from pyclimate.path import group_files_by_model_set, iter_matching_cmip5_file
 
 @pytest.mark.parametrize(('_filter', 'expected'), [
-    ([{'variable': 'tasmin'}], 5),
-    ([{'variable': 'tasmax'}], 6),
-    ([{'experiment': 'rcp85'}], 3)
+    ("[{'variable': 'tasmin'}]", 5),
+    ("[{'variable': 'tasmax'}]", 6),
+    ("[{'experiment': 'rcp85'}]", 3)
 ])
 def test_file_iter(cmip5_file_list, _filter, expected):
     i = iter_matching_cmip5_file(cmip5_file_list, _filter)
